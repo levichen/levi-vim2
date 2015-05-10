1a8b3ca543005652acdc82818c2ac102481d1453
levichen <jiunyi.c@gmail.com>
2015-05-10 03:21:13 +0000

    add set up shell

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+current=`pwd`
+
+echo $current
+echo $HOME
+
+if [ -L "$HOME/.vim"  ] || [ -d "$HOME/.vim"  ]
+then
+   mv $HOME/.vim $HOME/.vim.bak
+fi
+if [ -L "$HOME/.vimrc"  ] || [ -f "$HOME/.vimrc"  ]
+then
+    mv $HOME/.vimrc $HOME/.vimrc.bak
+fi
+
+cd $HOME
+ln -s $current $HOME/.vim
+ln -s $current/mvimrc $HOME/.vimrc
+
+echo "Done"
