75672bbc32791453744d1e5112beda5a14f8317f
levichen <jiunyi.c@gmail.com>
2015-05-10 03:23:16 +0000

    add set up tmux shell

--- setup.sh
+++ setup.sh
@@ -2,9 +2,6 @@
 
 current=`pwd`
 
-echo $current
-echo $HOME
-
 if [ -L "$HOME/.vim"  ] || [ -d "$HOME/.vim"  ]
 then
    mv $HOME/.vim $HOME/.vim.bak
@@ -14,8 +11,14 @@ then
     mv $HOME/.vimrc $HOME/.vimrc.bak
 fi
 
+if [ -L "/root/.tmux.conf"  ] || [ -f "/root/.tmux.conf"  ]
+then
+    mv $HOME/.tmux.conf $HOME/.tmux.bak
+fi
+
 cd $HOME
 ln -s $current $HOME/.vim
-ln -s $current/mvimrc $HOME/.vimrc
+ln -s $current/vimrc $HOME/.vimrc
+ln -s $current/tmux.conf $HOME/.tmux.conf
 
 echo "Done"
