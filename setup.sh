93660c4b9218625c80d98a1af66fce87520d11a2
levichen <levichen@nine-yi.com>
2016-03-13 21:19:43 +0800

    remove tmux

--- setup.sh
+++ setup.sh
@@ -11,14 +11,8 @@ then
     mv $HOME/.vimrc $HOME/.vimrc.bak
 fi
 
-if [ -L "/root/.tmux.conf"  ] || [ -f "/root/.tmux.conf"  ]
-then
-    mv $HOME/.tmux.conf $HOME/.tmux.bak
-fi
-
 cd $HOME
 ln -s $current $HOME/.vim
 ln -s $current/vimrc $HOME/.vimrc
-ln -s $current/tmux.conf $HOME/.tmux.conf
 
 echo "Done"
